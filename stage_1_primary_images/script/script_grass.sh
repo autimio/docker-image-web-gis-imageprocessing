a3bf7cb83edfc8fda7876ff892f008fba282ebbc
Autímio Filho <autimio.filho@sgs.com>
2019-01-07 15:18:55 -0200

    fix

--- stage_1_primary_images/script/script_grass.sh
+++ stage_1_primary_images/script/script_grass.sh
@@ -1,11 +1,8 @@
-echo $PASSWORD | docker login -u $USERNAME --password-stdin
+export BUILD_DATE=$(date '+%d/%m/%Y %T UTC' -u)
+docker pull $USERNAME/$CONTAINER_IMAGE_2:latest || true
 
 build --cache-from $USERNAME/$CONTAINER_IMAGE_2:latest --build-arg BUILD_DATE="${BUILD_DATE}" --build-arg TRAVIS_COMMIT="${TRAVIS_COMMIT}" --tag $USERNAME/$CONTAINER_IMAGE_2:$TRAVIS_COMMIT --tag $USERNAME/$CONTAINER_IMAGE_2:latest -f stage_1_primary_images/Dockerfile.grass .
 
 docker images
-
-export BUILD_DATE=$(date '+%d/%m/%Y %T UTC' -u)
-
-docker pull $USERNAME/$CONTAINER_IMAGE_2:latest || true
-
+echo $PASSWORD | docker login -u $USERNAME --password-stdin
 docker push $USERNAME/$CONTAINER_IMAGE_2:$TRAVIS_COMMIT && docker push $USERNAME/$CONTAINER_IMAGE_2:latest
--- stage_1_primary_images/script/script_postgresql.sh
+++ stage_1_primary_images/script/script_postgresql.sh
@@ -1,11 +1,8 @@
-echo $PASSWORD | docker login -u $USERNAME --password-stdin
+export BUILD_DATE=$(date '+%d/%m/%Y %T UTC' -u)
+docker pull $USERNAME/$CONTAINER_IMAGE_3:latest || true
 
 docker build --cache-from $USERNAME/$CONTAINER_IMAGE_3:latest --build-arg BUILD_DATE="${BUILD_DATE}" --build-arg TRAVIS_COMMIT="${TRAVIS_COMMIT}" --tag $USERNAME/$CONTAINER_IMAGE_3:$TRAVIS_COMMIT --tag $USERNAME/$CONTAINER_IMAGE_3:latest -f Dockerfile.postgresql .
 
 docker images
-
-export BUILD_DATE=$(date '+%d/%m/%Y %T UTC' -u)
-
-docker pull $USERNAME/$CONTAINER_IMAGE_3:latest || true
-
+echo $PASSWORD | docker login -u $USERNAME --password-stdin
 docker push $USERNAME/$CONTAINER_IMAGE_3:$TRAVIS_COMMIT && docker push $USERNAME/$CONTAINER_IMAGE_3:latest
